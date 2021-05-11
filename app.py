d07725420094a131bd1705a56e8366558b88095b
nraminno <53029192+nraminno@users.noreply.github.com>
2021-05-11 16:17:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ import os
 from werkzeug.utils import secure_filename
 import cv2
 
-
-#IMAGE_FOLDER = os.path.join('static', 'images')
+IMAGE_FOLDER = os.path.join('static', 'images')
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-#app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
+app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
 os.makedirs(os.path.join(app.instance_path, 'images'), exist_ok=True)
 
 loaded_model = load_model('cnn_model.h5')
@@ -25,11 +24,11 @@ def predict():
     uploaded_file = request.files['file']
     print(uploaded_file)
     print(uploaded_file.filename)
-    full_filename =os.path.join(app.instance_path, 'images', secure_filename(uploaded_file.filename))
+    full_filename =os.path.join(app.config['UPLOAD_FOLDER'], 'images', secure_filename(uploaded_file.filename))
     print(full_filename)
     if uploaded_file.filename != '':
         filename=uploaded_file.filename
-        uploaded_file.save(os.path.join(app.instance_path, 'images', secure_filename(uploaded_file.filename)))
+        uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'images', secure_filename(uploaded_file.filename)))
     img = cv2.imread(full_filename,cv2.IMREAD_GRAYSCALE)
     img1=cv2.resize(img,(88,88))
     x_in=img1.reshape(88,88,1)
